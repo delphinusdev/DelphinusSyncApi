48e3798e4bce941a9f0ae579593ece2489d68b5d
Raquel Camacho Mejia <rcm_530423@hotmail.com>
2025-09-08 18:05:02 +0000

    mypictures optimizado en su procedimiento

--- samreportes/slim4/src/Services/FotosAppStagingService.php
+++ samreportes/slim4/src/Services/FotosAppStagingService.php
@@ -297,7 +297,7 @@ class FotosAppStagingService
     public function mypicturesStoreProcedure(string $fechad, string $fechah, array $location): array
     {
         $params =   array(':param0' => $location[0], ':param1' => 'app', ':param2' => null, ':param3' => null, ':param4' => 0);
-        $pedidosResult = $this->repo->select("EXEC sp_GetMypicturesApp @location_id = :param0, @tipo = :param1, @fechad = :param2, @fechah = :param3,  @forceReadAll = :param4", $params);
+        $pedidosResult = $this->repo->select("EXEC sp_GetMypicturesAppOptimized @location_id = :param0, @tipo = :param1, @fechad = :param2, @fechah = :param3,  @forceReadAll = :param4", $params);
 
 
         // $pedidosResult = $this->repo->select("EXEC sp_GetMypicturesApp :param0,:param1", $params);
